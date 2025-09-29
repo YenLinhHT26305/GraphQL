5cfaff4be20bb28a7ea11251fc1b486b926ffd9d
YnLinh26305 <linhhtyl0@gmail.com>
2025-09-29 13:55:11 +0700

    Delete baitapgraphql directory

--- baitapgraphql/src/main/java/vn/iotstar/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.iotstar.repository;
-
-import vn.iotstar.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
