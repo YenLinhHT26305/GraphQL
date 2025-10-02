c093a9d749e009c88e8a8ba2f7287af04a46902d
YnLinh26305 <linhhtyl0@gmail.com>
2025-10-02 23:41:04 +0700

    Delete Graphql directory

--- Graphql/src/main/java/vn/iotstar/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.iotstar.repository;
-
-import vn.iotstar.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
